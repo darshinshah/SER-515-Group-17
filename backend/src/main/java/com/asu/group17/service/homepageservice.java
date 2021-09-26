5139dacb601855e1cccc09a06d3017c293e7ea9c
ptosh1704 <ptoshniw@gmail.com>
2021-09-25 19:15:27 -0700

    Deleted the file from the git repository

---
+++ backend/src/main/java/com/asu/group17/service/homepageservice.java
@@ -0,0 +1,14 @@
+package com.asu.group17.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homepageservice {
+	
+	@GetMapping("/greeting")
+	public String greetingApi() {
+		return "Hii, ASU";
+	}
+
+}
