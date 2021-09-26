56e2d8188ef7bf919eb95029f3ff363f4e46038e
vnaga2 <vnaga2@asu.edu>
2021-09-25 21:42:56 -0700

    Added folder components and Greeting component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Components/Greet'
 
 function App() {
   function openAboutUs(){
@@ -7,20 +8,6 @@ function App() {
   }
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <button>
         Home
       </button>
@@ -46,6 +33,8 @@ function App() {
       <button onClick={openAboutUs}>
         About Us
       </button>
+
+      <Greet></Greet>
 
     </div>
   );
