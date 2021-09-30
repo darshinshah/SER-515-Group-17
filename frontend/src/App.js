c6668be4641e8a147612437ff02aea9f0cb5f207
Neel Nishant <neel@neels-air.3500.dhcp.asu.edu>
2021-09-30 16:15:52 -0700

    Added beautification to the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import AboutUs from './Components/AboutUs';
 import Greet from './Components/Greet'
-
+import NavBarButton from './Components/NavBarButton';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navigation from './Components/Navigation';
+import Home from './Components/Home';
+import Teams from './Components/Teams';
+// import NavBarComp from './Components/NavBarComp';
 function App() {
-  function openAboutUs(){
-    console.log('About us clicked.');
-  }
+  
   return (
     <div className="App">
-      <button>
-        Home
-      </button>
+
       
-      <button>
-        Teams
-      </button>
-      <button>
-        Schedules
-      </button>
-      <button>
-        Points Table
-      </button>
-      <button>
-        Apply
-      </button>
-      <button>
-        Rules
-      </button>
-      <button>
-        Sponsors
-      </button>
-      <button onClick={openAboutUs}>
-        About Us
-      </button>
+      <BrowserRouter>
+        <div className="homepage-navbar">
+          <div className="homepage-navbar__header">Soccer Tournament</div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/teams" component={Teams}/>
+             {/* <Route path="/schedules" component={Schedules}/>
+             <Route path="/pointstable" component={PointsTable}/>
+             <Route path="/apply" component={Apply}/>
+             <Route path="/rules" component={Rules}/>
+             <Route path="/sponsors" component={Sponsors}/> */}
+             <Route path="/about" component={AboutUs}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+  </div>
+  
+  )
 
-      <Greet></Greet>
-
-    </div>
-  );
 }
 
 export default App;
