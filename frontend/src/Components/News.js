f908a3c408156a6e35600d3d76a85bd902d18f77
Neel Nishant <neel@Neels-MacBook-Air.local>
2021-10-01 11:42:05 -0700

    Added News content to the page

--- frontend/src/Components/News.js
+++ frontend/src/Components/News.js
@@ -9,7 +9,12 @@ const News = () => {
                 </div>
                 <div className="newspage__text">
                     <ul>
-                        <li>Applications for the tournament are closed. Keep an eye for the match schedules and the tickets.</li>
+                        <li>1 Oct 2021 (Reuters) - The English Men's soccer team will start a new annual international tournament on home soil starting in 2022, the national side announced on Thursday.</li>
+                        <li>The tournament will be staged in October month of  this year, with Arizona being joined by Sun devil and Colorado Soccer club, while a fourth competing nation will be announced soon, the team said in a statement.</li>
+                        <li>The invitational event will take place in the form of three double-header matches played across seven days in a round-robin format</li>
+                        <li>More details for the inaugural competition, including venues and ticket information, will be released in the coming weeks.</li>
+                        <li>The annual tournament will significantly boost England's international calendar and ensure Sarina Wiegman's squad regularly face world-class opposition, starting with the build-up to next summer's UEFA Women's EURO on home soil and the FIFA Women's World Cup in 2022.</li>
+
                     </ul>
                     
                 </div>
