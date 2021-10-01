0dfd1dd3840b5ba5ce98052e386ec00e0347ff60
Nimil Shah <nimilshah030599@gmail.com>
2021-09-30 17:30:30 -0700

    added about us page with css

--- frontend/src/Components/AboutUs.js
+++ frontend/src/Components/AboutUs.js
@@ -2,7 +2,33 @@ import './AboutUs.css'
 function AboutUs(){
 
     return(
-        <div className="page-margin">About Us!</div>
+
+        <div className="aboutpage">
+        <div className="aboutpage__content">
+        
+       
+        <p className="header">About Game Of Inches </p>
+        <p>The Game of Inches Soccer Tournament was started in 2019 by Neel Nishant and Darshin Shah. Both gentlemen are lifetime avid soccer players
+           and also professional organizers. </p>
+           <p>We brought the Game Of Inches Tournament to Central Arizona since Arizona state required one more option for a national platform. Such a large number of good groups fizzle since they lose the top players to the "huge clubs". Why? What do those large clubs have that others don't? The appropriate response is - access to national leagues. The enormous clubs play in "ECNL'' or the previous "DA" associations that offer players the chance to be seen on a public stage and contend at the most significant level and most clubs work in their own local area not knowing what the choices are or how to gain access to these leagues. The staff at Game of Inches has the experience and foundation to give admittance to these associations to players and groups to gain exposure at the most significant level.</p>
+           <p>WHEN: December 10, 2021</p>
+           <p> Where: Phoenix, AZ</p>
+           <p>FEES: u8-u9-u10 (7v7) $300; u10-u11-u12 (8v8) $400; u12-u13-u14-u15 (11v11) $650</p>
+           <p>APPLICATION DEADLINE: November 10, 2021. All applications received by the deadline will be evaluated equally in the selection process. Applications received after the deadline will automatically be placed on a waiting list.</p>
+
+
+
+
+
+        
+        
+        
+        </div>
+        </div>
+         
+        
     )
 }
+
+
 export default AboutUs;
