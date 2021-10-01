39b3de9fcb897185fbfaf377eac936e416976282
vnaga2 <vnaga2@asu.edu>
2021-10-01 13:06:17 -0700

    Cleaned all the warnings in the frontend and added keys to the player

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,16 +1,7 @@
 import './Home.css'
-import React, { useState } from 'react';
+import React from 'react';
 import LoginBox from './LoginBox';
-import Players from './Players';
 const Home = () => {
-    // const [tableValues, setTableValues] = useState('');
-    
-    // const players = {}
-    // const showOrHidPlayerHandler = (props) => {
-    //     if(props.showPlayer)
-    //         players = <Players />
-
-    // }
     return (
         <div className="homepage">
             <div className="homepage__content">
