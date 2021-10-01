76f5ee3870cddf08a639f724ae868f56a174337e
Neel Nishant <neel@Neels-MacBook-Air.local>
2021-10-01 11:28:12 -0700

    Added news page in the website

---
+++ frontend/src/Components/News.js
@@ -0,0 +1,20 @@
+import './News.css';
+const News = () => {
+
+    return (
+        <div className="newspage">
+            <div className="newspage__content">
+                <div className="header">
+                    News
+                </div>
+                <div className="newspage__text">
+                    <ul>
+                        <li>Applications for the tournament are closed. Keep an eye for the match schedules and the tickets.</li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+export default News;
