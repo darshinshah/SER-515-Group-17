82c57feba852fc27322ad63be22ef2e8d42a3ddb
darshinshah <dshah32@asu.edu>
2021-09-30 20:05:53 -0700

    Added proper links to sponsors

--- frontend/src/Components/Sponsors.js
+++ frontend/src/Components/Sponsors.js
@@ -20,7 +20,7 @@ function Sponsors(){
             <img height="200" width="300" src={"https://logos-world.net/wp-content/uploads/2020/05/Gatorade-Logo.png"}/>
         </a>
 
-        <a href="https://1000logos.net/wp-content/uploads/2018/08/GEICO-Logo.png">
+        <a href="https://www.geico.com/">
             <img height="200" width="300" src={"https://1000logos.net/wp-content/uploads/2018/08/GEICO-Logo.png"}/>
         </a>
 
