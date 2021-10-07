ceebb9c2147170920f08634f1ee003642c207e7f
ptosh1704 <ptoshniw@gmail.com>
2021-10-07 12:22:18 -0700

    refactored login service code

--- backend/src/main/java/com/asu/group17/service/LoginController.java
+++ backend/src/main/java/com/asu/group17/service/LoginController.java
@@ -1,81 +1,52 @@
 package com.asu.group17.service;
 
 import java.util.Iterator;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.asu.group17.model.Login;
-import com.asu.group17.model.Player;
-import com.asu.group17.repository.PlayerRepository;
 import com.asu.group17.repository.UserRepository;
 
 @RestController
-@RequestMapping("/api")
-public class UserController {
-	
-	@Autowired
-	private PlayerRepository playerRepository;
-	
+@RequestMapping("/v1")
+public class LoginController {
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	@GetMapping("/players")
-	public List<Player> getPlayers(){
-		return this.playerRepository.findAll();
-	}
-	
-	@PostMapping("/post")
-	public Player save(@RequestBody Player player) {
-		return this.playerRepository.save(player);
-	}
-	
+
 	@CrossOrigin(origins = "*")
-	@PostMapping("/savelogin")
+	@PostMapping("/signup")
 	public String saveUser(@RequestBody Login data) {
 		Iterable<Login> userlist = userRepository.findAll();
-		
-		
 		Iterator<Login> it = userlist.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			Login logindetail = it.next();
-			
-			if(logindetail.getEmail() != null && logindetail.getEmail().equals(data.getEmail()))
-				return "User is already present";
+			if (logindetail.getEmail() != null && logindetail.getEmail().equals(data.getEmail()))
+				return "User is already already registered! Please try sign in!";
 		}
-		
 		this.userRepository.save(data);
-		return "User registered";
-		
-		
+		return "User registered " + data.toString();
 	}
-	
-	
- 
+
 	@CrossOrigin(origins = "*")
 	@PostMapping("/login")
 	public String getUser(@RequestBody Login data) {
 		Iterable<Login> userlist = userRepository.findAll();
-		
+
 		Iterator<Login> it = userlist.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			Login logindetail = it.next();
-			if(logindetail.getEmail() != null && logindetail.getEmail().equals(data.getEmail()))
-				return "User found";
+			if (logindetail.getEmail() != null && logindetail.getEmail().equals(data.getEmail()))
+				return "User logged in";
 		}
-		
-		
-		return "User not found";
-		
-		
-		
+
+		return "User not regsitered";
+
 	}
-	
 
 }
