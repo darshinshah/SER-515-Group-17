ece0f0b1330fdf7b53ccf805c84efb49d21be833
Nimil Shah <nimilshah030599@gmail.com>
2021-10-14 16:18:40 -0700

    Created an API to fetch details of each Team playing in the Tournament

---
+++ backend/src/main/java/com/asu/group17/repository/TeamsRepository.java
@@ -0,0 +1,13 @@
+package com.asu.group17.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.asu.group17.model.Teams;
+
+@Repository
+public interface TeamsRepository extends JpaRepository<Teams, Integer>{
+
+
+
+}
