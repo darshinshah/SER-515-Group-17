f35fc52921a4c0b6d52a456112a4f40b736b733d
Neel Nishant <neel@Neels-MacBook-Air.local>
2021-10-26 18:12:51 -0700

    Added files for coach login page

---
+++ frontend/src/Components/CoachPage.js
@@ -0,0 +1,14 @@
+import './CoachPage.css'
+const CoachPage = () => {
+    return(
+        <div className="coachpage">
+            <div className="coachpage__content">
+
+            </div>
+            <div className="coachpage__footer">
+
+            </div>
+        </div>
+    )
+}
+export default CoachPage;
