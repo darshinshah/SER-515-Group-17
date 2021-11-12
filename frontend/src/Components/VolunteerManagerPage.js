7d8b614f465dc168193db9685f48e772ceae0805
darshinshah <dshah32@asu.edu>
2021-11-11 23:52:22 -0700

    added frontend functionality - volunteer manager page

--- frontend/src/Components/VolunteerManagerPage.js
+++ frontend/src/Components/VolunteerManagerPage.js
@@ -1,10 +1,22 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 const VolunteerManagerPage = () => {
 
-    return(
+    const [volunteersData, setVolunteersData] = useState([]);
+        
+    useEffect(() => {
+        axios.get("http://localhost:8082/allVolunteers", { "Content-Type": "application/json" }).then(
+            (response) => {
+                setVolunteersData(response.data);
+            }
+        )
+    }, []);
+
+    return (
         <div>
-            In Volunteer Manager Page
+            In Volunteer Page
         </div>
-    )
+        )
 }
 
 export default VolunteerManagerPage;
