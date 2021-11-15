4ab55968316d0b69c664abf1106a8b3912c17566
Neel Nishant <nnishant@asu.edu>
2021-11-14 21:34:00 -0700

    Created form for volunteer registration

--- frontend/src/Components/Volunteer.js
+++ frontend/src/Components/Volunteer.js
@@ -1,14 +1,118 @@
+import './Apply.css';
 import React from 'react';
+import axios from 'axios';
 export class Volunteer extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+                volunteerId:'',
+                name:'',
+                age:'',
+                emailId:'',
+                gender:'',
+                phoneNumber:'',
+                city:'',
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (e) =>{
+        const {name,value} = e.target
+        this.setState({[name]:value})
+    }
+
+    submitForm(e){
+
+        e.preventDefault();
+
+        const user = {
+                name:this.state.name,
+                age:this.state.age,
+                emailId:this.state.emailId,
+                gender:this.state.gender,
+                phoneNumber:this.state.phoneNumber,
+                city:this.state.city,
+        }
+        console.log(user);
+
+        axios({
+            method: "post",
+            url: "http://localhost:8082/saveVolunteer",
+            data: user,
+            headers: { "Content-Type": "application/json" },
+          }).then((response)=>{
+            console.log(response.data);
+            alert("Application Submitted");
+          })
+
 
     }
 
+
+
+
+
+
     render(){
         return(
-            <div>
-                In Volunteer Registration page
+            <div className="applypage">
+                <div className="applypage__content">
+                    <div className="header">
+                        Application for Volunteer
+                    </div>
+                    <div className="applypage__text">
+    
+                        <form className="applypage__form">                            
+                            <div className="subheader_separator subheader_main"> Information</div>
+                            <div className="applypage__form_row">
+                                <label>
+                                    Name:
+                                </label>
+                                <input type="text" name="name" value={this.state.name} onChange={this.handleChange}></input>
+                            </div>
+                            <div className="applypage__form_row">
+                                <label>
+                                    Email: 
+                                </label>
+                                <input type="text" name="emailId" value={this.state.emailId} onChange={this.handleChange}></input>
+                            </div>
+                            <div className="applypage__form_row">
+                                <label>
+                                    Phone Number
+                                </label>
+                                <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange}></input>
+                            </div>
+                            <div className="applypage__form_row">
+                                <label>
+                                    Gender
+                                </label>
+                                <input type="text" name="gender" value={this.state.gender} onChange={this.handleChange}></input>
+                           
+                            </div>
+                            <div className="applypage__form_row">
+                                <label>
+                                    age
+                                </label>
+                                <input type="text" name="age" value={this.state.age} onChange={this.handleChange}></input>
+                            </div>
+                           
+                            <div className="applypage__form_row">
+                                <label>
+                                    City
+                                </label>
+                                <input type="text" name="city" value={this.state.city} onChange={this.handleChange}></input>
+                            </div>
+                            
+                           
+                            <div className="applypage__form_row" style={{ paddingTop: "5rem" }}>
+    
+                                <input type="submit" value="Submit Form" style={{ width: "12rem" }} onClick={this.submitForm.bind(this)}/>
+                            </div>
+    
+    
+                        </form>
+                    </div>
+                </div>
             </div>
         )}
 }
