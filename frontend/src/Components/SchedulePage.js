a93d703bfed7b76e6c5746493d12873c64c91c99
vnaga2 <vnaga2@asu.edu>
2021-11-15 13:48:38 -0700

    Displayed fixtures on the tournamnet manager login

--- frontend/src/Components/SchedulePage.js
+++ frontend/src/Components/SchedulePage.js
@@ -1,3 +1,4 @@
+import './RefereeTablePage.css'
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 const SchedulePage = () => {
@@ -13,9 +14,36 @@ const SchedulePage = () => {
     }, []);
 
     return (
-        <div>
-            In Schedule page
+        <div className="refereetablepage">
+        <div className="refereetablepage__content">
+            <div className='app-container'>
+                <strong>List of Fixtures</strong>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Home Team</th>
+                            <th>Away Team</th>
+                            <th>Category</th>
+                            <th>Venue</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {scheduleData.map((data) => (
+                            <tr key={data.matchId}>
+                                <td>{data.homeTeam}</td>
+                                <td>{data.awayTeam}</td>
+                                <td>{data.category}</td>
+                                <td>{data.venue}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
+        <div className="refereetablepage__footer">
+
+        </div>
+    </div>
     )
 }
 
